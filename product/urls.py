1d7a7a0ed0d440fab4afef872128a9647234049b
behruz <xoliqberdiyevbehruz12@gmail.com>
2024-11-09 11:35:30 +0500

    feat(product): fix url name and path

--- product/urls.py
+++ product/urls.py
@@ -3,15 +3,20 @@ from django.urls import path
 from product import views
 
 urlpatterns = [
-    path('product/create/', views.ProductCreateApiView.as_view(), name='product-create'),
+    # product category
     path('product-category/list/', views.ProductCategoryListApiView.as_view(), name='product-category-list'),
-    path('list/', views.ProductCategoryListApiView.as_view(), name='product-category-list'),
+    # product
+    path('product/create/', views.ProductCreateApiView.as_view(), name='product-create'),
+    path('product/list/', views.ProductCategoryListApiView.as_view(), name='product-category-list'),
+    # cart-item
     path('cart-item/create/', views.ProductCartItemCreateApiView.as_view(), name='product-cart-item-create'),
     path('cart-item/<int:cart_item_id>/update/', views.ProductCartItemUpdateApiView.as_view(), name='product-cart-item-update'),
     path('cart-item/<int:id>/delete/', views.ProductCartItemDeleteApiView.as_view(), name='product-cart-item-delete'),
-    path('create-cart/', views.GetOrderApiView.as_view(), name='get_order'),
+    # cart
+    path('cart/create/', views.GetOrderApiView.as_view(), name='get_order'),
+    # order
     path('order/create/', views.ProductOrderCreateApiView.as_view(), name='product-order-create'),
     path('order/confirm/', views.ProductOrderConfirmApiView.as_view(), name='product-order-confirm'),
-    path('order/is-confirm/list/', views.ProductOrderIsConfirmApiView.as_view(), name='product-order-is-confirm'),
-    path('order/is-not-confirm/list/', views.ProductOrderIsNotConfirmApiView.as_view(), name='product-order-is-confirm'),
+    path('order/list/is-confirm/', views.ProductOrderIsConfirmApiView.as_view(), name='product-order-is-confirm'),
+    path('order/list/is-not-confirm/', views.ProductOrderIsNotConfirmApiView.as_view(), name='product-order-is-confirm'),
 ]
