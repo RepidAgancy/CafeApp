6ef000d7345225b4469c440e338bde79485fdb79
behruz <xoliqberdiyevbehruz12@gmail.com>
2024-11-09 11:34:58 +0500

    feat(common): fix url name and path

--- common/urls.py
+++ common/urls.py
@@ -1,24 +1,26 @@
-from tkinter.font import names
 
 from django.urls import path
 
 from common import views
 
 urlpatterns = [
-    path('tables/', views.TableListApiView.as_view(), name='tables'),
-    path('table/get/', views.TableGetApiView.as_view(), name='table-get'),
-    path('foods/', views.FoodListApiView.as_view(), name='foods'),
+    # table
+    path('table/list/', views.TableListApiView.as_view(), name='tables'),
+    path('table/choose/', views.TableGetApiView.as_view(), name='table-get'),
+    # food
+    path('food/list/', views.FoodListApiView.as_view(), name='foods'),
     path('food/<int:id>/', views.FoodDetailApiView.as_view(), name='food-detail'),
+    # cart-item
     path('cart-item/create/', views.CartItemCreateApiView.as_view(), name='cart-item-create'),
-    path('cart-item/<int:cart_item_id>/', views.CartItemEditApiView.as_view(), name='cart-item-edit'),
+    path('cart-item/<int:cart_item_id>/update/', views.CartItemEditApiView.as_view(), name='cart-item-edit'),
     path('cart-item/<int:cart_item_id>/delete/', views.CartItemDeleteApiView.as_view(),name='cart-item-delete'),
+    # cart
     path('cart/<int:id>/', views.CartDetailApiView.as_view(), name='cart-detail'),
+    # order
     path('order/create/', views.OrderCreateApiView.as_view(), name='order-create'),
-    path('order/confirm/', views.OrderConfirmedApiView.as_view(), name='order-confirmed'),
-    path('orders/in-process/', views.OrderListInProcessApiView.as_view(), name='orders'),
-    path('orders/is-done/', views.OrderListIsDoneApiView.as_view(), name='orders'),
+    path('order/confirm/', views.OrderConfirmedApiView.as_view(), name='order-confirm'),
+    path('order/list/in-process/', views.OrderListInProcessApiView.as_view(), name='order-list-in-process'),
+    path('order/list/is-done/', views.OrderListIsDoneApiView.as_view(), name='order-list-is-done'),
+    # finish day
     path('finish-day/', views.FinishDayApiView.as_view(), name='finish-day'),
-    # path('order-receipt/is-confimed/list/', views.OrderReceiptListIsConfirmedApiView.as_view(), name='order-reciept'),
-    # path('order-receipt/is-not-confimed/list/', views.OrderReceiptListIsNotConfirmedApiView.as_view(), name='order-reciept'),
-    # path('order-receipt/confim/', views.OrderReceiptConfirmApiView.as_view(), name='order-receipt'),
 ]
