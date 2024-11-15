0c526f736e4fffd2f4176cbcb8e64d6fc71d8a8c
ShahzodToy <abdashevshahzod340@gmail.com>
2024-11-15 16:26:25 +0500

    pagination added

--- core/settings.py
+++ core/settings.py
@@ -178,9 +178,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.MultiPartParser',
         'rest_framework.parsers.FormParser',
-    ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    ]
 }
 
 # simple jwt confi
