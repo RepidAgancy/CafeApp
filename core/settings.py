29532c46cc315ff94ccb85af62a145be4e1b3115
ShahzodToy <abdashevshahzod340@gmail.com>
2024-11-15 11:55:47 +0500

    corsheadres are added

--- core/settings.py
+++ core/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # packages
+    'corsheaders',
     'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt',
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -83,6 +85,27 @@ TEMPLATES = [
 ASGI_APPLICATION = 'core.asgi.application'
 WSGI_APPLICATION = 'core.wsgi.application'
 
+
+CSRF_TRUSTED_ORIGINS = ['http://crm.repid.uz/','http://127.0.0.1']
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:5173',  # Your frontend address
+
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # For local development
+    "http://127.0.0.1:8000",  # If you're testing with local servers
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
