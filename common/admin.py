c6c655ac4411bd32b51e9a284f1daa7dc6372caf
Behruz <xoliqbediyevbehruz@gmail.com>
2024-12-09 12:32:47 +0500

    fix

--- common/admin.py
+++ common/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from common import models
 
+@admin.register(models.Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('number', 'is_busy')
+    list_editable = ['is_busy']
 
-admin.site.register(models.Table)
+    def is_busy(self, obj):
+        return format_html('<input type="checkbox" {} disabled>', 'checked' if obj.is_busy else '')
+    is_busy.short_description = 'Checked Status'
+
+
 admin.site.register(models.CategoryFood)
 admin.site.register(models.Food)
 admin.site.register(models.Cart)
